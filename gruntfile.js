8523965ba6ef82b3b1dcf679d61ce57aeb9038c4
Andrew Smith <emertechie@gmail.com>
2014-09-22 14:04:36 +0700

    Initial commit

---
+++ gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        appName: 'SampleApp',
+        appDir: 'app',
+        cssDir: '<%= appDir %>/css',
+        sassDir: '<%= appDir %>/scss',
+        appScss: '<%= sassDir %>/styles.scss',
+        appCss: '<%= cssDir %>/styles.css',
+
+        sass: {
+            dev: {
+                options: {
+                    sourcemap: true
+                },
+                files: {
+                    '<%= appCss %>': '<%= appScss %>'
+                }
+            },
+            dist: {
+                files: {
+                    '<%= appCss %>': '<%= appScss %>'
+                }
+            }
+        },
+
+        jshint: {
+            files: [
+                '<%= appDir %>/**/*.js',
+                '!<%= appDir %>/bower_components/**',
+                'specs/**/*.js',
+                'tests/**/*.js',
+                'e2e-tests/**/*.js',
+                '!.*/**'
+            ],
+            options: {
+                reporter: require('jshint-stylish'),
+                laxbreak: true,
+                globals: {
+                    jQuery: true
+                }
+            }
+        },
+
+        watch: {
+            sass: {
+                files: ['<%= sassDir %>/**/*.scss', '<%= appDir %>/components/**/*.scss'],
+                tasks: ['sass:dev'],
+                options: {
+                    spawn: false,
+                    livereload: true
+                }
+            },
+            other: {
+                files: ['<%= appDir %>/**/*.html', '<%= appDir %>/**/*.js'],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    });
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('default', ['jshint', 'sass:dist']);
+};
